7e61d08553fca7860bdc329b7bcdc946df68b609
Rafal Pawlowski <rafal.pawlowski@creativelynx.ad>
2023-07-19 15:10:56 +0100

    feat: multiple select component and multiple query search

---
+++ src/components/Item.jsx
@@ -0,0 +1,13 @@
+const Item = ( { name, price, timeOfSale } ) => {
+
+  return(
+    <div className='flex flex-row gap-4 items-center justify-center'>
+      <img src={`https://render.albiononline.com/v1/item/${name}`} className='w-16 h-16' />
+      <p className='text-3xl'>{name}</p>
+      <p className='text-3xl'>{price}</p>
+      <p className='text-3xl'>{timeOfSale}</p>
+    </div>
+  )
+}
+
+export default Item;
