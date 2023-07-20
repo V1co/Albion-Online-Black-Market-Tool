1c29d1752eb83ab2341ae0af125488409ce5f6a3
Rafal Pawlowski <rafal.pawlowski@creativelynx.ad>
2023-07-20 09:49:38 +0100

    feat: dateFromNow util added to display hours

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,3 +1,5 @@
+import DateFromNow from "../utils/DateFromNow";
+
 const Item = ( { name, price, timeOfSale } ) => {
 
   return(
@@ -5,7 +7,7 @@ const Item = ( { name, price, timeOfSale } ) => {
       <img src={`https://render.albiononline.com/v1/item/${name}`} className='w-16 h-16' />
       <p className='text-3xl'>{name}</p>
       <p className='text-3xl'>{price}</p>
-      <p className='text-3xl'>{timeOfSale}</p>
+      <p className='text-3xl'>{DateFromNow(timeOfSale)}</p>
     </div>
   )
 }
