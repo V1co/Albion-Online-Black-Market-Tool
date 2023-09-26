f4853477c51c383f7369115e848905349952aa22
Rafal Pawlowski <rafal.pawlowski@creativelynx.ad>
2023-09-26 10:16:30 +0100

    feat: data was not checked within 24 hours

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -9,8 +9,8 @@ const Item = ( { callbackName, price, timeOfSale } ) => {
     <div className='flex flex-row gap-4 items-center justify-center'>
       <img src={`https://render.albiononline.com/v1/item/${callbackName}`} className='w-16 h-16' />
       <p className='text-3xl'>{itemsData[correctItemName]}</p>
-      <p className='text-3xl'>{price}</p>
-      <p className='text-3xl'>{DateFromNow(timeOfSale)}</p>
+      <p className='text-3xl'>{price === 0? 'was not checked within 24 hours' : price}</p>
+      {price === 0? '' : <p className='text-3xl'>{DateFromNow(timeOfSale)}</p>}
     </div>
   )
 }
